09931b12815a8219f2b394fb196709f75d42168f
lewis mcmillan <lewistmcmillan@gmail.com>
2022-12-07 13:20:31 +0000

    Added new gui which only has one window, with a tab system to swap between calibration and aquire mode

--- updater.m
+++ updater.m
@@ -1,6 +1,6 @@
 function [] = updater(obj, event, app)
     app.time = app.time + 1;
     time = string(duration(0,0,app.time));
-    app.TimetakenEditField.Value = time;
+    app.TimeTakenEditField.Value = time;
 end
 
