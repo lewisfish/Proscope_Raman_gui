edc16e6565a8cb480613a896cef7a93eaf42f81e
lewis mcmillan <lewistmcmillan@gmail.com>
2023-03-01 13:37:51 +0000

    Removed dummy payloads in laser.m. Added start of COMport check code. Added style guide. Start of format of Laser.m

--- Laser.m
+++ Laser.m
@@ -32,9 +32,9 @@ classdef Laser
 %           ERROR_CHECKSUM                  7
 
     properties
-        temperature {mustBeNumeric} % Target temperature of the laser. Default value is 25 C
-        current {mustBeNumeric} % Laser current. Default value is 40 mA
-        heater_current {mustBeNumeric} % Current for the laser heater drivers. This value is split evenly between the two drivers. Default value is 550mA
+        Temperature {mustBeNumeric} % Target temperature of the laser. Default value is 25 C
+        Current {mustBeNumeric} % Laser current. Default value is 40 mA
+        HeaterCurrent {mustBeNumeric} % Current for the laser heater drivers. This value is split evenly between the two drivers. Default value is 550mA
     end
     properties (Access = private, Constant)
         % private properties used internally so that "magic numbers" are removed
@@ -54,12 +54,12 @@ classdef Laser
     end
     properties (Access = private)
        % private properties used internally
-       sport % serial port
-       digital_temperature {mustBeNumeric}
-       digital_current {mustBeNumeric}
-       digital_heater_current0 {mustBeNumeric}
-       digital_heater_current1 {mustBeNumeric}
-       errors = ["NO_ERROR", "ERROR_TIMEOUT_BEFORE_1st_BYTE", "ERROR_TIMEOUT_BEFORE_2nd_BYTE","ERROR_NOT_ENOUGH_BYTES","ERROR_WRONG_CMD","","","ERROR_CHECKSUM"]
+       SerialPort % serial port
+       DigitalTemperature {mustBeNumeric}
+       DigitalCurrent {mustBeNumeric}
+       DigitalHeaterCurrent0 {mustBeNumeric}
+       DigitalHeaterCurrent1 {mustBeNumeric}
+       Errors = ["NO_ERROR", "ERROR_TIMEOUT_BEFORE_1st_BYTE", "ERROR_TIMEOUT_BEFORE_2nd_BYTE","ERROR_NOT_ENOUGH_BYTES","ERROR_WRONG_CMD","","","ERROR_CHECKSUM"]
     end
     methods
         function obj = Laser(temperature, current, heater_current)
@@ -71,101 +71,102 @@ classdef Laser
                 current = 40
                 heater_current = 550
             end
-            obj.temperature = temperature;
-            obj.current = current;
-            obj.heater_current = heater_current;
+            obj.Temperature = temperature;
+            obj.Current = current;
+            obj.HeaterCurrent = heater_current;
             % convert analogue to "digital". Conversions taken from documentation.
-            obj.digital_temperature = (obj.temperature * 1140) - 3207;
-            obj.digital_current = obj.current * 134.5;
-            obj.digital_heater_current0 = 218 * (obj.heater_current / 2.);
-            obj.digital_heater_current1 = 218 * (obj.heater_current / 2.);
+            obj.DigitalTemperature = (obj.Temperature * 1140) - 3207;
+            obj.DigitalCurrent = obj.Current * 134.5;
+            obj.DigitalHeaterCurrent0 = 218 * (obj.HeaterCurrent / 2.);
+            obj.DigitalHeaterCurrent1 = 218 * (obj.HeaterCurrent / 2.);
     
-            % https://uk.mathworks.com/matlabcentral/answers/110249-how-can-i-identify-com-port-devices-on-windows
-            % https://superuser.com/questions/835848/how-to-view-serial-com-ports-but-not-through-device-manager
+
+            devices = IDSerialComs();
+            deviceNames = devices[:, 1];
+            COMPort = devices[:, 2];
+            
             % open communication to the laser and set variables on device
-%             obj.sport = serialport('COM4', 38400, 'DataBits', 8);
+            obj.SerialPort = serialport('COM4', 38400, 'DataBits', 8);
             % check if it is always com4 if not to write code to check for siliconlabs as per
             % datasheet
-            % try this https://uk.mathworks.com/matlabcentral/answers/110249-how-can-i-identify-com-port-devices-on-windows
-            obj.set_temp();
-            obj.set_current();
-            obj.set_heater_current();
-            obj.enable_TEC();
-            obj.check_ready();
-            % obj.wait_for_stable();
-%             obj.enable_laser_heater_power();
+            obj.setTemperature();
+            obj.setCurrent();
+            obj.setHeaterCurrent();
+            obj.enableTEC();
+            obj.checkReady();
+            obj.enableLaserHeaterPower();
         end
-        function obj = set_temp(obj)
-            % Set target temperature of laser in C
+        function obj = setTemperature(obj)
+            % Set target temperature of laser in Celcius
             msg = [0x2a, 0x06, obj.UART_TMP_SET];
-            bytes_vector = obj.dec2bytes(obj.digital_temperature);
+            bytes_vector = obj.dec2Bytes(obj.DigitalTemperature);
             msg = [msg, bytes_vector];
-            msg = [msg, obj.get_checksum(msg)];
-%             write(obj.sport, msg,'uint8');
-%             data = read(obj.sport,4,"uint8"); % message recieved should be 0x2a 0x04 0x35 0x1b
+            msg = [msg, obj.getChecksum(msg)];
+            write(obj.SerialPort, msg,'uint8');
+            data = read(obj.SerialPort,4,"uint8"); % message recieved should be 0x2a 0x04 0x35 0x1b
 
 %             data = [0x2a, 0x05, 0x00, 0x01, 0x2a]; % dummy data with wrong checksum
 %             data = [0x2a, 0x05, 0x00, 0x01, 0x2e]; % dummy data with an error code. Need to check this is actually what is produced.
-            data = [0x2a, 0x04, 0x35, 0x1b]; % dummy data
-            obj.check_error(data, obj.UART_TMP_SET, 'set temperature');
+%             data = [0x2a, 0x04, 0x35, 0x1b]; % dummy data
+            obj.checkError(data, obj.UART_TMP_SET, 'set temperature');
         end
         
-        function obj = set_current(obj)
+        function obj = setCurrent(obj)
             % Set the the laser current in mA
             msg = [0x2a, 0x06, obj.UART_LASER_CURRENT];
-            bytes_vector = obj.dec2bytes(obj.digital_current);
+            bytes_vector = obj.dec2Bytes(obj.DigitalCurrent);
             msg = [msg, bytes_vector];
-            msg = [msg, obj.get_checksum(msg)];
-%             write(obj.sport, msg, 'uint8');
-%             data = read(obj.sport,4,'uint8'); % message recieved should be 0x2a 0x04 0x13 0x3d
-            data = [0x2a, 0x04, 0x13, 0x3d];  % dummy data
-            obj.check_error(data, obj.UART_LASER_CURRENT, 'set current');
+            msg = [msg, obj.getChecksum(msg)];
+            write(obj.SerialPort, msg, 'uint8');
+            data = read(obj.SerialPort,4,'uint8'); % message recieved should be 0x2a 0x04 0x13 0x3d
+%             data = [0x2a, 0x04, 0x13, 0x3d];  % dummy data
+            obj.checkError(data, obj.UART_LASER_CURRENT, 'set current');
         end
         
-        function obj = set_heater_current(obj)
+        function obj = setHeaterCurrent(obj)
             % Set the heater current for driver 0 and 1 in mA
             % ultimately sets the laser wavelength.
             msg = [0x2a, 0x6, obj.UART_I_HEAT0];
-            bytes_vector0 = obj.dec2bytes(obj.digital_heater_current0);
+            bytes_vector0 = obj.dec2Bytes(obj.DigitalHeaterCurrent0);
             msg = [msg, bytes_vector0];
-            msg = [msg, obj.get_checksum(msg)];
-            %write(obj.sport, msg, 'uint8');
-%             data = read(obj.sport,4,'uint8'); % message recieved should be 0x2a 0x04 0x22 0x0c
-            data = [0x2a, 0x04, 0x22, 0x0c];  % dummy data
-            obj.check_error(data, obj.UART_I_HEAT0, 'set heater current 0');
+            msg = [msg, obj.getChecksum(msg)];
+            write(obj.SerialPort, msg, 'uint8');
+            data = read(obj.SerialPort,4,'uint8'); % message recieved should be 0x2a 0x04 0x22 0x0c
+%             data = [0x2a, 0x04, 0x22, 0x0c];  % dummy data
+            obj.checkError(data, obj.UART_I_HEAT0, 'set heater current 0');
             
    
             msg = [0x2a, 0x6, obj.UART_I_HEAT1];
-            bytes_vector1 = obj.dec2bytes(obj.digital_heater_current1);
+            bytes_vector1 = obj.dec2Bytes(obj.DigitalHeaterCurrent1);
             msg = [msg, bytes_vector1];
-            msg = [msg, obj.get_checksum(msg)];
-            %write(obj.sport, msg, 'uint8');
-%             data = read(obj.sport,4,'uint8'); % message recieved should be 0x2a 0x04 0x23 0x0d
-            data = [0x2a, 0x04, 0x23, 0x0d];  % dummy data
-            obj.check_error(data, obj.UART_I_HEAT1, 'set heater current 1');
+            msg = [msg, obj.getChecksum(msg)];
+            write(obj.SerialPort, msg, 'uint8');
+            data = read(obj.SerialPort,4,'uint8'); % message recieved should be 0x2a 0x04 0x23 0x0d
+%             data = [0x2a, 0x04, 0x23, 0x0d];  % dummy data
+            obj.checkError(data, obj.UART_I_HEAT1, 'set heater current 1');
         end
         
-        function obj = enable_TEC(obj)
+        function obj = enableTEC(obj)
             % Turn on the peltier element
             msg = [0x2a, 0x05, obj.UART_EN_TEC];
             bytes = uint8(hex2dec(obj.ON));
             msg = [msg, bytes];
-            msg = [msg, obj.get_checksum(msg)];
-            %write(obj.sport, msg, 'uint8');
-%             data = read(obj.sport,4,'uint8'); % message recieved should be 0x2a 0x04 0x30 0x1e
-            data = [0x2a, 0x04, 0x30, 0x1e]; % dummy data
-            obj.check_error(data, obj.UART_EN_TEC, 'enable TEC');
+            msg = [msg, obj.getChecksum(msg)];
+            write(obj.SerialPort, msg, 'uint8');
+            data = read(obj.SerialPort,4,'uint8'); % message recieved should be 0x2a 0x04 0x30 0x1e
+%             data = [0x2a, 0x04, 0x30, 0x1e]; % dummy data
+            obj.checkError(data, obj.UART_EN_TEC, 'enable TEC');
         end
         
-        function obj = check_ready(obj)
+        function obj = checkReady(obj)
             % Check device is ready to be turned on. Waits until confirmation message is recieved.
             msg = [0xaa, 0x05, obj.UART_DEVICE_STATE];
-            msg = [msg, obj.get_checksum(msg)];
+            msg = [msg, obj.getChecksum(msg)];
             while 1
-                %write(obj.sport, msg, 'uint8');
-                %data = read(obj.sport,4,'uint8'); % message recieved should be 0xaa 0x05 0x05 0x02 0xa8
-                data = [0xaa, 0x05, 0x05, 0x02, 0xa8]; % dummy data
-                obj.check_error(data, obj.UART_DEVICE_STATE, 'check ready');
+                write(obj.SerialPort, msg, 'uint8');
+                data = read(obj.SerialPort,5,'uint8'); % message recieved should be 0xaa 0x05 0x05 0x02 0xa8
+%                 data = [0xaa, 0x05, 0x05, 0x02, 0xa8]; % dummy data
+                obj.checkError(data, obj.UART_DEVICE_STATE, 'check ready');
 
                 %check message returns 0x02 at position data(4)
                 if data(4) == 0x02
@@ -173,85 +174,137 @@ classdef Laser
                 end
             end
         end
-        
-        function obj = wait_for_stable(obj)
-            msg = [0xaa, 0x04, obj.UART_TMP];
-            msg = [msg, obj.get_checksum(msg)];
-            while 1
-                write(obj.sport, msg, 'uint8');
-                data = read(obj.sport, 6,'uint8');
-                temp = data(4:5);
-                if cond == true
-                    break
-                end
-                pause(0.5); % sleep for 0.5s and repeat
-            end
-        end
-        
-        function obj = enable_laser_heater_power(obj)
+                
+        function obj = enableLaserHeaterPower(obj)
             % Turn on the laser heater
             msg = [0x2a, 0x05, obj.UART_EN_HEAT_PWR];
             bytes = uint8(hex2dec(obj.ON));
             msg = [msg, bytes];
-            msg = [msg, obj.get_checksum(msg)];
-            %write(obj.sport, msg, 'uint8');
-%             data = read(obj.sport,4,'uint8'); % message recieved should be 0x2a 0x04 0x20 0x0e
-            data = [0x2a, 0x04, 0x20, 0x0e];  % dummy data
-            obj.check_error(data, obj.UART_EN_HEAT_PWR, 'enable laser heater power');
+            msg = [msg, obj.getChecksum(msg)];
+            write(obj.SerialPort, msg, 'uint8');
+            data = read(obj.SerialPort,4,'uint8'); % message recieved should be 0x2a 0x04 0x20 0x0e
+%             data = [0x2a, 0x04, 0x20, 0x0e];  % dummy data
+            obj.checkError(data, obj.UART_EN_HEAT_PWR, 'enable laser heater power');
         end
         
-        function obj = switch_off(obj)
+        function obj = switchOff(obj)
             % Turn off laser and heater
             msg = [0x2a, 0x05, obj.UART_EN_HEAT_PWR];
             bytes = uint8(hex2dec(obj.OFF));
             msg = [msg, bytes];
-            msg = [msg, obj.get_checksum(msg)];
-            %write(obj.sport, msg, 'uint8');
-%             data = read(obj.sport,4,'uint8'); % message recieved should be 0x2a 0x04 0x20 0x0e
-            data = [0x2a, 0x04, 0x20, 0x0e]; % dummy data
-            obj.check_error(data, obj.UART_EN_HEAT_PWR, 'disable laser heater power');
+            msg = [msg, obj.getChecksum(msg)];
+            write(obj.SerialPort, msg, 'uint8');
+            data = read(obj.SerialPort,4,'uint8'); % message recieved should be 0x2a 0x04 0x20 0x0e
+%             data = [0x2a, 0x04, 0x20, 0x0e]; % dummy data
+            obj.checkError(data, obj.UART_EN_HEAT_PWR, 'disable laser heater power');
 
             msg = [0x2a, 0x05, obj.UART_EN_TEC];
             bytes = uint8(hex2dec(obj.OFF));
             msg = [msg, bytes];
-            msg = [msg, obj.get_checksum(msg)];
-            %write(obj.sport, msg, 'uint8');
-%             data = read(obj.sport,4,'uint8'); % message recieved should be 0x2a 0x04 0x30 0x1e
-            data = [0x2a, 0x04, 0x30, 0x1e]; % dummy data
-            obj.check_error(data, obj.UART_EN_TEC, 'disable TEC');
+            msg = [msg, obj.getChecksum(msg)];
+            write(obj.SerialPort, msg, 'uint8');
+            data = read(obj.SerialPort,4,'uint8'); % message recieved should be 0x2a 0x04 0x30 0x1e
+%             data = [0x2a, 0x04, 0x30, 0x1e]; % dummy data
+            obj.checkError(data, obj.UART_EN_TEC, 'disable TEC');
         end
         
-        function out = get_error(obj, error_code)
+        function out = getError(obj, error_code)
             % Retreive error code message
             out = obj.errors(error_code+1);
         end
         
-        function obj = check_error(obj, msg, cmd, location)
-           if obj.get_checksum(msg(1:length(msg)-1)) ~= msg(end)
-               error('Checksum recieved after %s is wrong!', location);
+        function obj = checkError(obj, msg, cmd, location)
+           if obj.getChecksum(msg(1:length(msg)-1)) ~= msg(end)
+               err_msg = sprirtf('Checksum recieved after %s is wrong',location);
+               uiwait(errordlg(err_msg, "Error"));
             end
             % check if error is raised
             if msg(3) ~= cmd
-                disp(msg)
                 err_code = obj.get_error(msg(4));
-                error('In %s, device returned the following error: %s',location,err_code);
+                err_msg = sprirtf('In %s, device returned the following error: %s',location,err_code);
+                uiwait(errordlg(err_msg, "Error"));
             end
         end
         
     end
     methods (Static)
-        function out = dec2bytes(val)
+        function out = dec2Bytes(val)
             % Convert decimal to hexstring then to uint8 vector
 %             https://uk.mathworks.com/matlabcentral/answers/391490-how-can-i-represent-a-hexadecimal-string-in-a-128-bit-format-in-matlab
             hexstr = dec2hex(val);
             byte_hex = string(permute(reshape(hexstr, 2, 2),[2 1]))';
             out = uint8(hex2dec(byte_hex));
         end
-        function checksum = get_checksum(msg)
+        function checksum = getChecksum(msg)
             % Compute the checksum of a message. Carries out bitwise XOR of message e.g a^b^c^d for message msg = [a, b, c, d]
             checksum = bitxor(msg(1), msg(2));
             for n = 3:length(msg)
                 checksum = bitxor(checksum, msg(n));
+            end
+        end
+        function devices = IDSerialComs()
+            % taken from here: https://uk.mathworks.com/matlabcentral/fileexchange/45675-identify-serial-com-devices-by-friendly-name-in-windows
+            % IDSerialComs identifies Serial COM devices on Windows systems by friendly name
+            % Searches the Windows registry for serial hardware info and returns devices,
+            % a cell array where the first column holds the name of the device and the
+            % second column holds the COM number. Devices returns empty if nothing is found.
+
+            devices = [];
+
+            Skey = 'HKEY_LOCAL_MACHINE\HARDWARE\DEVICEMAP\SERIALCOMM';
+            [~, list] = dos(['REG QUERY ' Skey]);
+            if ischar(list) && strcmp('ERROR',list(1:5))
+                disp('Error: IDSerialComs - No SERIALCOMM registry entry')
+                return;
+            end
+            list = strread(list,'%s','delimiter',' '); %#ok<FPARK> requires strread()
+            coms = 0;
+            for i = 1:numel(list)
+                if strcmp(list{i}(1:3),'COM')
+                    if ~iscell(coms)
+                        coms = list(i);
+                    else
+                        coms{end+1} = list{i}; %#ok<AGROW> Loop size is always small
+                    end
+                end
+            end
+            key = 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\USB\';
+            [~, vals] = dos(['REG QUERY ' key ' /s /f "FriendlyName" /t "REG_SZ"']);
+            if ischar(vals) && strcmp('ERROR',vals(1:5))
+                disp('Error: IDSerialComs - No Enumerated USB registry entry')
+                return;
+            end
+            vals = textscan(vals,'%s','delimiter','\t');
+            vals = cat(1,vals{:});
+            out = 0;
+            for i = 1:numel(vals)
+                if strcmp(vals{i}(1:min(12,end)),'FriendlyName')
+                    if ~iscell(out)
+                        out = vals(i);
+                    else
+                        out{end+1} = vals{i}; %#ok<AGROW> Loop size is always small
+                    end
+                end
+            end
+
+            for i = 1:numel(coms)
+                match = strfind(out,[coms{i},')']);
+                ind = 0;
+                for j = 1:numel(match)
+                    if ~isempty(match{j})
+                        ind = j;
+                    end
+                end
+                if ind ~= 0
+                    com = str2double(coms{i}(4:end));
+                    if com > 9
+                        length = 8;
+                    else
+                        length = 7;
+                    end
+                    devices{i,1} = out{ind}(27:end-length); %#ok<AGROW>
+                    devices{i,2} = com; %#ok<AGROW> Loop size is always small
+                end
             end
         end
     end
--- README.md
+++ README.md
@@ -2,3 +2,70 @@
 Matlab code to contorl the laser, spectrometer and grating for the Proscope project.
 
 ![Raman GUI](Raman_gui.png)
+
+# Style Guide
+
+The style guide has been loosly based uponthe exported m file from the app designer code and augmented with Google's C++ [style guide](https://google.github.io/styleguide/cppguide.html).
+Clean code by Robert Cecil Martin has also been followed.
+
+## Variable names
+
+lower case, snake case
+
+```
+table_name = "table 1";
+CCD_temperature = 70 % degrees celsius;
+```
+
+### Constants
+
+Upper case whole word, snake case. Like the following:
+
+```
+A_CONSTANT = 10;
+UART_ERROR = (0x03);
+```
+
+## Functions
+
+Camel case (lowercase first word) in the following style:
+
+```
+function myOutput = myFunction(x)
+.
+.
+.
+end
+```
+
+## Classes
+
+## Class Names
+
+Camel case (uppercase first word) in the following style
+
+```
+classdef BasicClass
+.
+.
+.
+end
+```
+
+## Class functions
+
+Camel case (lower case first word) in the following style:
+
+```
+app.createComponents()
+app.checkReady()
+```
+
+## Class Members
+
+Camel case (uppercase first word) in the following style:
+
+```
+app.AquireButton = ...
+app.SavePathButton = ...
+```
